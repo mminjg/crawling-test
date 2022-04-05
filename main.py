d7043e3ea55523c4e5e05a0e13fb5e2bee4c689e
Kang Minji <mminjg@gmail.com>
2022-04-05 22:23:57 +0900

    :sparkles: 크롤링 link 정보 추가

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 
 # dotenv
 load_dotenv(verbose=True)
+WEBDRIVER_PATH = os.getenv('WEBDRIVER_PATH')
 MYSQL_HOST = os.getenv('MYSQL_HOST')
 MYSQL_USER = os.getenv('MYSQL_USER')
 MYSQL_PW = os.getenv('MYSQL_PW')
@@ -14,7 +15,7 @@ MYSQL_DB = os.getenv('MYSQL_DB')
 MYSQL_CHARSET = os.getenv('MYSQL_CHARSET')
 
 # yes24 크롤링
-driver = webdriver.Chrome(executable_path="/Users/user/Downloads/chromedriver")
+driver = webdriver.Chrome(executable_path=WEBDRIVER_PATH)
 url = 'http://ticket.yes24.com/New/Notice/NoticeMain.aspx'
 driver.get(url)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
@@ -25,9 +26,10 @@ for tr in soup.find_all('tr')[1:]:
     if type != "티켓오픈":
         continue
     title = tr.select('td')[1].text.strip()
+    link = url + tr.select('td')[1].find('a')['href']
     dateStr = tr.select('td')[2].text.strip()
     date = re.sub('\((월|화|수|목|금)\)', '', dateStr).replace('.','-')
-    info_list.append((title, date, "YES24"))
+    info_list.append((title, date, link, "YES24"))
 
 # db 연결
 conn = pymysql.connect(host=MYSQL_HOST,
@@ -36,9 +38,9 @@ conn = pymysql.connect(host=MYSQL_HOST,
                        db=MYSQL_DB,
                        charset=MYSQL_CHARSET)
 
-sql = "INSERT INTO open_info (title, date, site) VALUES (%s, %s, %s)"
+sql = "INSERT INTO open_info (title, date, link, site) VALUES (%s, %s, %s, %s)"
 with conn:
     with conn.cursor() as cur:
-        for title, date, site in info_list:
-            cur.execute(sql, (title, date, site))
+        for title, date, link, site in info_list:
+            cur.execute(sql, (title, date, link, site))
             conn.commit()
